6bc34defd8af285e0bd70f2fcf5b0c082f1d5ce9
Mrehman84 <tengku_mann@yahoo.com>
2026-08-03 12:51:00 +0800

    tukar bahagiain ke wangan garis 13

--- menu_kewangan.py
+++ menu_kewangan.py
@@ -10,7 +10,8 @@ def get_gsheet_client():
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
-    creds_dict = st.secrets["gcp_service_account"]
+    creds_dict = st.secrets["gcp_service_account_finance"]
+
     creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
     return gspread.authorize(creds)
 
